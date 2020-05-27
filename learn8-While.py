150770bb8e72c09d873e33d7e2ed110ba8c4b878
Variabel_00 <zsatriawanz@gmail.com>
2020-05-27 10:28:14 +0700

    Membuat file perulanagn While dan ForIn

---
+++ learn8-While.py
@@ -0,0 +1,14 @@
+"""
+	---Loop
+	----while
+"""
+
+count = 1
+
+while count <= 5 :
+	print("Yay, Anankku akhirnya mau menikah")
+	count+=1
+#else ini jarang digunkanan artinya gunkan atau tidak terserah kita
+#fungsinnya untk keterangan dari while(akhir)
+else :
+	print("Perulangan sudah ",count)
---
+++ learn9-ForIn.py
@@ -0,0 +1,15 @@
+"""
+	---Loop
+	---- For In ==> untuk mengerurka isi list
+"""
+list1 = ['Satriawan','Suparlan','Hasanawadi','Awan Syaputra']
+
+#nama adalah variabel bebas
+for nama in list1 :
+	print("Halo, apa kabar ", nama)
+
+
+#Contoh lain
+list2 = "Python atau Cobra"
+for punk in list2 :
+	print(punk)
