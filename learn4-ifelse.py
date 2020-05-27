38103302d4dc6ec2dc07dfafb5d6b7409c18578b
Variabel_00 <zsatriawanz@gmail.com>
2020-05-27 08:06:40 +0700

    menambah file if else

---
+++ learn4-ifelse.py
@@ -0,0 +1,9 @@
+
+angka1 = 10
+angka2 = 25
+
+if angka1 < angka2 :
+	#untuk menuliskan statcment harus tab spasi atau tida boleh sejajar dengan if atau elsenya
+	print("Angka 1 lebih kecil dari angka 2")
+else:
+	print("Angka 1 lebih besar dari angka 2")
