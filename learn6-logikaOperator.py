77b9f026a30120ea28dcf514e6876e6273b51dfc
Variabel_00 <zsatriawanz@gmail.com>
2020-05-27 09:58:17 +0700

    menambahkan file Logika Operator

---
+++ learn6-logikaOperator.py
@@ -0,0 +1,15 @@
+"""
+ 	Jika syarat lebih dari satu syarat
+ 	and		or		not
+ 	&		|		!=
+
+
+"""
+#True atau False tulis dengan hurup awal kapital
+syarat1 = True
+syarat2 = False
+
+if syarat1 & syarat2:
+	print("Anda Benar")
+else :
+	print("Anda Salah")
---
+++ learn7-contohLogikaOperator.py
@@ -0,0 +1,15 @@
+"""
+	Latijan : Putri sedang mencari jodoh
+	syarat : baik dan rajin
+"""
+tamu = "cewek"
+baik = True
+rajin = True
+
+if baik & rajin:
+	if tamu == "pria":
+		print("Nikah Yuk")
+	else:
+		print("Kita Saudaraan saja")
+else:
+	print("Hush Sana!!")
